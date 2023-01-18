9a49f7e1217084608827dcefab133f4e545617a8
Mark Mikhailov <pprimak@mail.ru>
2023-01-18 19:58:54 +0200

    Add mapVew, buttons

---
+++ RouteTestTask(iOS-Junior)/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  TestTask(iOS-Junior)
+//
+//  Created by Марк Михайлов on 18.01.2023.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    let mapView : MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    let addAdressButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "addAdress"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "route"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    let resetButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "reset"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setConstraints()
+        
+        addAdressButton.addTarget(self, action: #selector(addAdressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(roteButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    @objc func addAdressButtonTapped() {
+        print("TapAdd")
+    }
+    
+    @objc func roteButtonTapped() {
+        print("TapRoute")
+    }
+    
+    @objc func resetButtonTapped() {
+        print("TapReset")
+    }
+}
+
+extension ViewController {
+    func setConstraints() {
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        mapView.addSubview(addAdressButton)
+        NSLayoutConstraint.activate([
+            addAdressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAdressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAdressButton.heightAnchor.constraint(equalToConstant: 70),
+            addAdressButton.widthAnchor.constraint(equalToConstant: 70)
+            ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 55),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -40),
+            routeButton.heightAnchor.constraint(equalToConstant: 70),
+            routeButton.widthAnchor.constraint(equalToConstant: 70)
+            ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -55),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -40),
+            resetButton.heightAnchor.constraint(equalToConstant: 70),
+            resetButton.widthAnchor.constraint(equalToConstant: 70)
+            ])
+
+    }
+}
